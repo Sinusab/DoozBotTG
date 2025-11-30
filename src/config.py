5a26c42e49eccb5e7a37728cf9c30510c497b922
Sina Abyar <134528508+Sinusab@users.noreply.github.com>
2025-11-30 21:05:49 +0330

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# src/config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN not found! Add it to your .env file.")
